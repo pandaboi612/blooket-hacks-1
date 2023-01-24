353c258bbf45cd8d0aef5257a301888883e33518
Jude <judergideon@gmail.com>
2023-01-24 09:45:55 -0600

    fix addRewards.js

--- unobfuscated/global/addRewards.js
+++ unobfuscated/global/addRewards.js
@@ -39,17 +39,21 @@ function discordFooter() {
   
   if (!location.href.includes("play.blooket.com")) alert("Please go to play.blooket.com and run script again.");
              else {
-               var tokenAdder = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-               tokenAdder
-                 .get("https://play.blooket.com/api/users/me").then(({ data: { name } }) => {
-                 tokenAdder
-                   .get("https://play.blooket.com/api/users/bonuses").then(({ data: { tokensAvailable: addedTokens, xpAvailable: addedXp } }) => {
-                   tokenAdder
-                     .put("https://play.blooket.com/api/users/add-rewards", { name, addedTokens, addedXp })
-                           .then(() => alert(`Added ${addedTokens} tokens and ${addedXp} xp!`))
-                           .catch(() => alert('There was an error when adding rewards.'));
-                   }).catch(() => alert("There was an error getting bonus data."));
-               }).catch(() => alert('There was an error user data.'));
+                    var state = Object.values(document.querySelector("#app > div > div"))[1].children[1]._owner.stateNode.state;
+                   const tokenadder = Object.values(webpackJsonp.push([ [], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']], ]).cache).find((x) => x.exports ?.a?.put).exports.a;
+            tokenadder.get("https://play.blooket.com/api/users/me").then(a => {
+                tokenadder.put("https://play.blooket.com/api/users/add-rewards", {
+          name: a.data.name,
+          addedTokens: 500,
+          addedXp: 300
+        })});
+       if (state.xpAvailable === undefined) {
+        state.xpAvailable = 0
+      } if (state.tokensAvailable === undefined) {
+        state.tokensAvailable = 0
+      }
+
+      alert('Added maximum rewards!');
              };
 
 console.log(
