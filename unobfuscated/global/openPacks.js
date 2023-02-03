09209b1cb4b3837dc5545f1fac13e9bdd1314b5b
Jude <judergideon@gmail.com>
2023-02-03 11:55:29 -0600

    Update openPacks.js

--- unobfuscated/global/openPacks.js
+++ unobfuscated/global/openPacks.js
@@ -38,37 +38,44 @@ function discordFooter() {
   
 discordFooter();
 
-var openPacks = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
-
-openPacks.get("https://dashboard.blooket.com/api/users").then(async ({ data: { name, tokens } }) => {
-    let prices = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find(x => x?.exports?.a?.Safari).exports.a || {
-        Medieval: 15, Breakfast: 15, Wonderland: 15, Space: 20, Bot: 20, Aquatic: 20, Safari: 20, Dino: 25, "Ice Monster": 25, Blizzard: 25, Outback: 25
-    };;
-    let pack = prompt("Which pack would you like to open? (ex: \"Ice Monster\")").split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ');
-    if (!Object.keys(prices).map(x => x.toLowerCase()).includes(pack.toLowerCase())) return alert("I couldn't find that pack...");
-
-    let amount = Math.min(Math.floor(tokens / Object.entries(prices).find(x => x[0].toLowerCase() == pack.toLowerCase())[1]), parseInt(`0${prompt("How many packs would you like to open?")}`));
-    if (amount == 0) return alert("You do not have enough tokens to afford this.");
-
-    let alertOnOpen = confirm("Would you like to be alerted of your unlocks?");
-    let blooks = {};
-    let now = Date.now();
-    let error = false;
-
-    for (let i = 0; i < amount; i++) {
-        await openPacks.put("https://dashboard.blooket.com/api/users/unlockblook",
-                            { name, pack })
-          .then(({ data: { unlockedBlook, tokens, isNewBlook } }) => {
-            blooks[unlockedBlook] ||= 0;
-            blooks[unlockedBlook]++;
-
-            let before = Date.now();
-
-            if (alertOnOpen) alert(`${unlockedBlook} (${i + 1}/${amount}) ${isNewBlook ? "NEW! " : ''}${tokens} tokens left`);
-
-            now += Date.now() - before;
-        }).catch(e => error = true);
-        if (error) break;
-    }
-    alert(`(${Date.now() - now}ms) Results:\n${Object.entries(blooks).map(([blook, amount]) => `    ${blook} ${amount}`).join(`\n`)}`);
-}).catch(() => alert('There was an error with your user data...'));
+            var open = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find((x) => x.exports?.a?.get).exports.a;
+            
+            open.get("https://dashboard.blooket.com/api/users").then(async ({ data: { name, tokens } }) => {
+                let prices = Object.values(webpackJsonp.push([[], { ['']: (_, a, b) => { a.cache = b.c }, }, [['']],]).cache).find(x => x?.exports?.a?.Safari).exports.a || {
+                    Medieval: 15,
+                    Breakfast: 15,
+                    Wonderland: 15,
+                    Space: 20,
+                    Bot: 20,
+                    Aquatic: 20,
+                    Safari: 20,
+                    Dino: 25,
+                    "Ice Monster": 25,
+                    Outback: 25
+                };;
+                let box = prompt("Which box do you want to open? (ex: \"Outback\")").split(' ').map(str => str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()).join(' ');
+                if (!Object.keys(prices).map(x => x.toLowerCase()).includes(box.toLowerCase())) return alert("I couldn't find that box...");
+            
+                let amount = Math.min(Math.floor(tokens / Object.entries(prices).find(x => x[0].toLowerCase() == box.toLowerCase())[1]), parseInt(`0${prompt("How many boxes do you want to open?")}`));
+                if (amount == 0) return alert("You do not have enough tokens...");
+            
+                let alertBlooks = confirm("Would you like to alert blooks upon unlocking?");
+                let blooks = {};
+                let now = Date.now();
+                let error = false;
+            
+                for (let i = 0; i < amount; i++) {
+                    await open.put("https://dashboard.blooket.com/api/users/unlockblook", { name, box }).then(({ data: { unlockedBlook, tokens, isNewBlook } }) => {
+                        blooks[unlockedBlook] ||= 0;
+                        blooks[unlockedBlook]++;
+            
+                        let before = Date.now();
+            
+                        if (alertBlooks) alert(`${unlockedBlook} (${i + 1}/${amount}) ${isNewBlook ? "NEW! " : ''}${tokens} tokens left`);
+            
+                        now += Date.now() - before;
+                    }).catch(e => error = true);
+                    if (error) break;
+                }
+                alert(`Results:\n${Object.entries(blooks).map(([blook, amount]) => `    ${blook} ${amount}`).join(`\n`)}`);
+            }).catch(() => alert('There was an error getting user data!'));
